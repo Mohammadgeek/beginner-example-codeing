9f2f401512f4485583f2e347b1d4713270d7de84
Mohammadreza Helali <crestin38@hotmail.com>
2022-08-03 20:11:02 +0430

    Update dom.js

--- trickcode/dom.js
+++ trickcode/dom.js
@@ -1,6 +1,15 @@
-#remove item
+//remove item
 
 document.querySelector('#div-1').remove()
 document.querySelector('#div-1').parentElement.remove()
 
+/* 
+if you are remove last child from div 
+*/
 
+function removelist(){
+  const list = document.getElementById("div-2");
+  if (list.hasChildNodes()){
+    list.removeChild(list.lastChild);
+  }
+}
